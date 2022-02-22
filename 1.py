121e4b901093889ca1b50c1463fe82b67923bd5f
Мызников Евгений <junaers1996@gmail.com>
2022-02-22 21:33:12 +0300

    use input

--- 1.py
+++ 1.py
@@ -1,26 +1,27 @@
 import requests
 import json
-from pprint import pprint
 from tqdm import tqdm
 
 class vk_api():
+
     def __init__(self):
         pass
 
     token_vk = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+    id_vk = input('Укажите id в Вконтакте - ')
+    params = {
+        'owner_id': id_vk,
+        'album_id': 'profile',
+        'extended': '1',
+        'photo_sizes': '1',
+        'access_token': token_vk,
+        'v': '5.131'
+    }
     URL = 'https://api.vk.com/method/photos.get'
 
-    def get_max_size_photos_url(self, id_vk):
-        params = {
-            'owner_id': id_vk,
-            'album_id': 'profile',
-            'extended': '1',
-            'photo_sizes': '1',
-            'access_token': self.token_vk,
-            'v': '5.131'
-        }
+    def get_max_size_photos_url(self):
         count = 0
-        res = requests.get(self.URL, params=params)
+        res = requests.get(self.URL, params=self.params)
         sizes = res.json()['response']['items']
         size = []
         max_size = []
@@ -33,17 +34,9 @@ class vk_api():
             max_size.append(si[5]['url'])
         return max_size
 
-    def get_like_name(self, id_vk):
-        params = {
-            'owner_id': id_vk,
-            'album_id': 'profile',
-            'extended': '1',
-            'photo_sizes': '1',
-            'access_token': self.token_vk,
-            'v': '5.131'
-        }
+    def get_like_name(self):
         count = 0
-        res = requests.get(self.URL, params=params)
+        res = requests.get(self.URL, params=self.params)
         likes = (res.json()['response']['items'])
         name = []
         for like in tqdm(likes):
@@ -61,34 +54,33 @@ class vk_api():
 vk = vk_api()
 
 class yandex:
-    def __init__(self):
+    token = input('Укажите токен своего яндекс диска - ')
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': '{}'.format(f'{token}')
+    }
+
+    def __init__(self):\
         pass
 
-    def get_folder(self, token):
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': '{}'.format(token)
-        }
+
+    def get_folder(self):
         params = {
             'path': 'vk'
         }
-        response = requests.put('https://cloud-api.yandex.net/v1/disk/resources', headers=headers, params=params)
+        response = requests.put('https://cloud-api.yandex.net/v1/disk/resources', headers=self.headers, params=params)
         response.json()
 
-    def download_file_for_url(self, url, path, token):
-        headers = {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': '{}'.format(token)
-        }
+    def download_file_for_url(self, url, path):
+        yan.get_folder()
         data2 = []
         for ur, pat in tqdm(zip(url, path)):
                 params = {
                     'url': ur,
                     'path': f'vk/{pat}'
                 }
-                response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload', headers=headers, params=params)
+                response = requests.post('https://cloud-api.yandex.net/v1/disk/resources/upload', headers=self.headers, params=params)
                 data = [{
                     'file_name': pat,
                     'size': 'w'
@@ -98,17 +90,12 @@ class yandex:
                     json.dump(data2, f, ensure_ascii=False, indent=2)
         return print('Cкачивание завершино')
 
-class save_foto(vk_api,yandex):
-    def __init__(self):
-        pass
+yan = yandex()
+yan.download_file_for_url(vk.get_max_size_photos_url(), vk.get_like_name())
 
-    def programm(self, id, token):
-        yan = yandex()
-        yan.get_folder(token)
-        yan.download_file_for_url(vk.get_max_size_photos_url(id), vk.get_like_name(id), token)
 
-start = save_foto()
-start.programm('492994968', '')
+
+
 
 
 
